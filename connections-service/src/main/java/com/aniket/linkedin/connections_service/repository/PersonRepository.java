1799f80ba34b46fcdbdf2be3cff5c425be05f58c
Aniket Kadukar <aniket.kadukar81@gmail.com>
2024-10-20 12:04:23 +0530

    Added all the files to git

---
+++ connections-service/src/main/java/com/aniket/linkedin/connections_service/repository/PersonRepository.java
@@ -0,0 +1,24 @@
+package com.aniket.linkedin.connections_service.repository;
+
+
+import com.aniket.linkedin.connections_service.entity.Person;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonRepository extends Neo4jRepository<Person,Long> {
+
+    Optional<Person> getByName(String name);
+
+    @Query("MATCH (personA:Person) -[:CONNECTED_TO]- (personB:Person) " +
+            "WHERE id(personA)=$userId " +
+            "RETURN personB")
+    List<Person> getFistDegreeConnections(Long userId);
+
+    @Query("MATCH (personA:Person) -[:CONNECTED_TO]- (personB:Person) " +
+            "WHERE id(personA)=$userId " +
+            "RETURN personB")
+    List<Person> getSecondDegreeConnections(Long userId);
+}
---
+++ discovery-server/src/test/java/com/aniket/linkedin/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniket.linkedin.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
